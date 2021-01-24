3a41a2866d12ff755e161f46300b132f211f18bb
Anders Steen <astee@kth.se>
2021-01-24 19:42:13 +0100

    create project, implement typing

---
+++ TypesMart/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+    public Item currentItem;
+
+    private Dictionary<KeyCode, char> alphabet;
+
+    void Start() {
+        InitializeAlphabet();
+    }
+
+    void OnGUI() {
+        Event e = Event.current;
+        
+        if (e.isKey
+            && e.type == EventType.KeyDown
+            && e.keyCode != KeyCode.None &&
+            currentItem.SendCharacter(alphabet[e.keyCode])) {
+            
+            Destroy(currentItem.gameObject);
+        }
+    }
+
+    void InitializeAlphabet() {
+        alphabet = new Dictionary<KeyCode, char>();
+        alphabet.Add(KeyCode.Space, '_');
+
+        KeyCode[] keyCodes = (KeyCode[]) Enum.GetValues(typeof(KeyCode));
+
+        foreach (KeyCode code in keyCodes) {
+            string keyString = code.ToString().ToLower();
+            if (keyString.Length == 1) {
+                alphabet.Add(code, keyString[0]);
+            }
+        }
+    }
+}
---
+++ TypesMart/Assets/Scripts/Item.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[ExecuteAlways]
+public class Item : MonoBehaviour {
+   
+    private int currentIndex = 0;
+    private string itemName = "sample_im";
+    private TMP_Text backText;
+    private TMP_Text frontText;
+
+    void Start() {
+        //RectTransform background = transform.Find("Background").GetComponent<RectTransform>();
+        //background.sizeDelta = new Vector2(characterWidth * itemName.Length + 2 * margin, height);
+
+        //backText = transform.Find("Background/BackText").GetComponent<TMP_Text>();
+        frontText = transform.Find("Background/FrontText").GetComponent<TMP_Text>();
+
+        //backText.text = itemName;
+        frontText.text = itemName;
+
+        //frontText.GetComponent<RectTransform>().sizeDelta = frontText.GetTextBounds();//new Vector2(characterWidth * itemName.Length, height * 0.5f);
+    }
+
+    public bool SendCharacter(char character) {
+        if (itemName[currentIndex] == character) {
+            currentIndex++;
+            SetFrontText();
+
+            return currentIndex == itemName.Length;
+        }
+        return false;
+    }
+
+    private void SetFrontText() {
+        frontText.text = itemName.Substring(currentIndex);
+    }
+}
